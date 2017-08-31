eed0694b648429001f4459c65fa63e6e9a194987
Hunter Rancourt <skwid138@gmail.com>
2017-08-31 17:02:41 -0500

    completed assignment

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ function onReady() {
   // Your code here
 
   // Make R red
-
+  $('.red').css('color', 'red');
   // Make O orange
-
+  $('.orange').css('color', 'orange');
   // Make Y yellow
-
+  $('.yellow').css('color', 'yellow');
   // Make G green
-
+  $('#green').css('color', 'green');
   // Make B blue
-
+  $('div span:first-child').css('color', 'blue');
   // Make I indigo
-
+  $('div span:nth-child(2)').css('color', 'indigo');
   // Make V violet
+  $('div span:last-child').css('color', 'violet');
 }
